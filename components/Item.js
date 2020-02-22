bb3f8edf27ebfb673078571bbccbcc03a0543153
kaba <taikikabaya@gmail.com>
2020-02-22 16:24:38 +0900

    refactor: add hooks to Item.js

--- components/Item.js
+++ components/Item.js
@@ -1,105 +1,98 @@
+import React, { useState, useEffect } from 'react'
 import { Ionicons } from '@expo/vector-icons'
-import React from 'react'
 import { Image, StyleSheet, Text, View, TouchableHighlight } from 'react-native'
 import { increaseNiceCount } from '../firebase/Fire'
 
 const profileImageSize = 36
 const padding = 12
 
-export default class Item extends React.Component {
-  state = {}
+const Item = props => {
+  const [_width, setWidth] = useState(null)
+  const [_height, setHeight] = useState(null)
 
-  componentDidMount () {
-    if (!this.props.imageWidth) {
+  useEffect(() => {
+    if (props.imageWidth) {
       // Get the size of the web image
-      Image.getSize(this.props.image, (width, height) => {
-        this.setState({ width, height })
+      Image.getSize(props.image, (width, height) => {
+        setWidth(width)
+        setHeight(height)
       })
     }
-  }
+  }, [])
 
-  render () {
-    const {
-      contentId,
-      text,
-      name,
-      imageWidth,
-      imageHeight,
-      uid,
-      image,
-      niceCount
-    } = this.props
-    console.log({ props: this.props })
+  const {
+    contentId,
+    text,
+    name,
+    imageWidth,
+    imageHeight,
+    uid,
+    image,
+    niceCount
+  } = props
 
-    // Reduce the name to something
-    const imgW = imageWidth || this.state.width
-    const imgH = imageHeight || this.state.height
-    const aspect = imgW / imgH || 1
+  // Reduce the name to something
+  const imgW = imageWidth || _width
+  const imgH = imageHeight || _height
+  const aspect = imgW / imgH || 1
 
-    return (
-      <View>
-        <Header image={{ uri: image }} name={name} />
-        <Image
-          resizeMode='contain'
-          style={{
-            backgroundColor: '#D8D8D8',
-            width: '100%',
-            aspectRatio: aspect
-          }}
-          source={{ uri: image }}
-        />
-        <Metadata
-          contentId={contentId}
-          niceCount={niceCount}
-          name={name}
-          description={text}
-        />
-      </View>
-    )
-  }
-}
-
-const Metadata = ({ name, description, contentId, niceCount }) => {
   return (
-    <View style={styles.padding}>
-      <IconBar contentId={contentId} niceCount={niceCount} />
-      <Text style={styles.text}>{name}</Text>
-      <Text style={styles.subtitle}>{description}</Text>
+    <View>
+      <View style={[S.row, S.padding]}>
+        <View style={S.row}>
+          <Image style={S.avatar} source={{ uri: image }} />
+          <Text style={S.text}>{name}</Text>
+        </View>
+        <Ionicons style={S.icon} name={'ios-more'} size={26} color='black' />
+      </View>
+
+      <Image
+        resizeMode='contain'
+        style={S.image(aspect)}
+        source={{ uri: image }}
+      />
+      <View style={S.padding}>
+        <View style={S.row}>
+          <View style={S.row}>
+            <TouchableHighlight
+              onPress={() => increaseNiceCount(contentId, niceCount)}
+            >
+              <Ionicons
+                style={S.icon}
+                name={'ios-heart-empty'}
+                size={26}
+                color='black'
+              />
+            </TouchableHighlight>
+            <Ionicons
+              style={S.icon}
+              name={'ios-chatbubbles'}
+              size={26}
+              color='black'
+            />
+            <Ionicons
+              style={S.icon}
+              name={'ios-send'}
+              size={26}
+              color='black'
+            />
+          </View>
+          <Ionicons
+            style={S.icon}
+            name={'ios-bookmark'}
+            size={26}
+            color='black'
+          />
+        </View>
+
+        <Text style={S.text}>{name}</Text>
+        <Text style={S.subtitle}>{text}</Text>
+      </View>
     </View>
   )
 }
 
-const Header = ({ name, image }) => (
-  <View style={[styles.row, styles.padding]}>
-    <View style={styles.row}>
-      <Image style={styles.avatar} source={image} />
-      <Text style={styles.text}>{name}</Text>
-    </View>
-    <Icon name='ios-more' />
-  </View>
-)
-
-const Icon = ({ name }) => (
-  <Ionicons style={{ marginRight: 8 }} name={name} size={26} color='black' />
-)
-
-const IconBar = ({ contentId, niceCount }) => (
-  <View style={styles.row}>
-    <View style={styles.row}>
-      <TouchableHighlight
-        onPress={() => increaseNiceCount(contentId, niceCount)}
-      >
-        <Icon name='ios-heart-empty' />
-      </TouchableHighlight>
-      <Text>{niceCount}</Text>
-      <Icon name='ios-chatbubbles' />
-      <Icon name='ios-send' />
-    </View>
-    <Icon name='ios-bookmark' />
-  </View>
-)
-
-const styles = StyleSheet.create({
+const S = StyleSheet.create({
   text: { fontWeight: '600' },
   subtitle: {
     opacity: 0.8
@@ -122,5 +115,15 @@ const styles = StyleSheet.create({
     height: profileImageSize,
     resizeMode: 'cover',
     marginRight: padding
-  }
+  },
+  icon: {
+    marginRight: 8
+  },
+  image: aspect => ({
+    backgroundColor: '#D8D8D8',
+    width: '100%',
+    aspectRatio: aspect
+  })
 })
+
+export default Item
