162dae135664b7917efe5a8550a34ff90b6a7f24
kaba <taikikabaya@gmail.com>
2020-02-29 14:10:48 +0900

    feature: add user name  edit feature

--- screens/UserPageScreen.js
+++ screens/UserPageScreen.js
@@ -1,10 +1,52 @@
-import React from 'react'
-import { Image, TextInput, View, Text, StyleSheet } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import {
+  Image,
+  TextInput,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Alert
+} from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'
+import { getUserName, saveUserName } from '../asyncStorage/userStorage'
 
 const MyPageScreen = () => {
+  const [_name, setName] = useState('')
+  const [_isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setInitialState()
+  }, [])
+
+  setInitialState = async () => {
+    const userName = await getUserName()
+    console.log({ userName })
+    setName(userName)
+    setIsLoading(false)
+  }
+
+  const onChangeText = name => {
+    setName(name)
+    saveUserName(name)
+  }
+
+  if (_isLoading) return null
   return (
     <View style={S.container}>
-      <Text>my page</Text>
+      <View>
+        <TouchableOpacity onPress={() => Alert.alert('この機能は準備中です')}>
+          <MaterialIcons
+            style={{ backgroundColor: 'transparent' }}
+            name={'account-circle'}
+            color={'gray'}
+            size={110}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <TextInput value={_name} onChangeText={text => onChangeText(text)} />
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
