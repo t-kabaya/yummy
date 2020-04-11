e8bd1feb5b54ba9a8422f87fff9dc1546ecfbbbf
kaba <taikikabaya@gmail.com>
2020-04-11 17:39:43 +0900

    style: add new human image for no image user

--- screens/UserPageScreen.tsx
+++ screens/UserPageScreen.tsx
@@ -1,28 +1,24 @@
 import React, { useState, useEffect } from 'react'
 import {
   Image,
-  TextInput,
   View,
   StyleSheet,
   TouchableOpacity,
   FlatList,
   Dimensions,
   Platform,
-  StatusBar,
-  TouchableHighlight
+  StatusBar
 } from 'react-native'
 import Text from '../components/Text.tsx'
-import { MaterialIcons } from '@expo/vector-icons'
 import { getUserName } from '../asyncStorage/userStorage'
 import {
   getUserPosts,
-  uploadUserInfosAsync,
   getUserOwnIcon
 } from '../firebase/Fire'
-import getPermission from '../utils/getPermission'
 import color from '../assets/color'
 import { editUserProfileText } from '../assets/constant/text.ts'
 import store from '../store/store'
+import { widthPercentageToDP as wp} from 'react-native-responsive-screen'
 
 const { width } = Dimensions.get('window')
 
@@ -69,12 +65,7 @@ export default props => {
         {_userIcon ? (
           <Image source={{ uri: _userIcon }} style={S.userIcon} />
         ) : (
-          <MaterialIcons
-            style={{ backgroundColor: 'transparent' }}
-            name={'account-circle'}
-            color={'gray'}
-            size={110}
-          />
+          <Image source={require('../assets/human.png')} style={S.humanIcon} />
         )}
         <Text style={S.userNameText}>{_name}</Text>
       </View>
@@ -86,7 +77,12 @@ export default props => {
       <FlatList
         style={S.listContainer}
         data={_userPosts}
-        renderItem={({ item }) => <ListItem item={item} />}
+        renderItem={({ item }) => 
+        <View style={S.listItemContainer}>
+        <Image source={{ uri: item.image }} style={S.listItemImage} />
+        </View>}
+        // <ListItem item={item} />
+      // }
         numColumns={3}
       />
     </View>
@@ -99,22 +95,21 @@ const S = StyleSheet.create({
     padding: 10,
     marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
     backgroundColor: color.profileBack
-    // flexDirection: 'col',
   },
   userNameText: {
-    marginTop: width * 0.05,
+    marginTop: wp('5%'),
     marginLeft: 5,
     fontSize: 15
   },
   listItemContainer: {
-    width: width / 3
+    width: wp('33%')
   },
   listContainer: {
     marginTop: 15
   },
   listItemImage: {
-    width: width / 3 - 1,
-    height: width / 3 - 1
+    width: wp('32%'),
+    height: wp('32%')
   },
   userIcon: {
     height: 100,
@@ -125,16 +120,20 @@ const S = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     alignSelf: 'center',
-    width: width * 0.9,
+    width: wp('90%'),
     height: 30,
     borderRadius: 10,
     borderWidth: 1,
     backgroundColor: 'white',
     borderColor: 'gray',
-    marginTop: width * 0.07
+    marginTop: wp('7%')
   },
   editProfileButtonText: {
-    fontSize: width * 0.03,
+    fontSize: wp('3%'),
     fontWeight: 'bold'
+  },
+  humanIcon: {
+    width: wp('18%'),
+    height: wp('18%')
   }
 })
