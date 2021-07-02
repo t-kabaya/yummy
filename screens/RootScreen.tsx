5857148af90dffa4538eaf857e750634c8bc8a60
kaba <taikikabaya@gmail.com>
2021-07-02 13:24:34 +0900

    console.disableYellowBox = trueを、LogBoxへと変更

--- screens/RootScreen.tsx
+++ screens/RootScreen.tsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react'
-import { SafeAreaView, StyleSheet, View} from 'react-native'
+import { SafeAreaView, StyleSheet, LogBox} from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import * as Font from 'expo-font'
 import RootRoute from '../routing/RootRoute'
 import Store from '../state/Store'
-console.disableYellowBox = true
+LogBox.ignoreAllLogs()
 
 export default () => {
   // const [_isLoading, setIsLoading] = useState(true)
