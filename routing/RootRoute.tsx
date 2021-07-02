c530bcefb0c019562ee4939a48c06ac22c17dc25
kaba <taikikabaya@gmail.com>
2021-07-02 13:10:51 +0900

    expo42への移行が完了した。しかしアイコンが表示されない。

--- routing/RootRoute.tsx
+++ routing/RootRoute.tsx
@@ -2,18 +2,30 @@ import * as React from 'react'
 // import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 //import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import { View, Text } from 'react-native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import tabBarIcon from '../components/tabBarIcon'
 import FeedStack from './FeedStack'
 import MyPageStack from './MyPageStack'
 import SelectImageStack from './SelectImageStack'
-import { RootStackParamList  } from 'type'
+import { BottomTabParamList  } from 'type'
 
-const Stack = createStackNavigator<RootStackParamList>()
+const BottomTab = createBottomTabNavigator<BottomTabParamList>();
 
 export default () => (
-  <Stack.Navigator screenOptions={{ headerShown: false }}>
-    <Stack.Screen name="Root" component={FeedStack} />
-  </Stack.Navigator>
+  <BottomTab.Navigator
+    initialRouteName="Feed">
+    <BottomTab.Screen
+      name="Feed"
+      component={FeedStack}
+    />
+    <BottomTab.Screen
+      name="SelectImage"
+      component={SelectImageStack}
+    />
+    <BottomTab.Screen
+      name="MyPage"
+      component={MyPageStack}
+    />
+  </BottomTab.Navigator>
 )
--- type/index.ts
+++ type/index.ts
@@ -1,4 +1,10 @@
 export type RootStackParamList = {
-    Root: undefined;
-    NotFound: undefined;
+  Root: undefined;
+  NotFound: undefined;
+}
+
+export type BottomTabParamList = {
+  Feed: undefined;
+  SelectImage: undefined;
+  MyPage: undefined;
 }
