871956b9012a76ff559d0e1657d09f905d46919b
kaba <taikikabaya@gmail.com>
2021-07-02 13:21:42 +0900

    タブバーの文字を変更した。

--- routing/RootRoute.tsx
+++ routing/RootRoute.tsx
@@ -1,10 +1,7 @@
 import * as React from 'react'
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-//import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
-import tabBarIcon from '../components/tabBarIcon'
+import TabBarIcon from '../components/tabBarIcon'
 import FeedStack from './FeedStack'
 import MyPageStack from './MyPageStack'
 import SelectImageStack from './SelectImageStack'
@@ -18,14 +15,26 @@ export default () => (
     <BottomTab.Screen
       name="Feed"
       component={FeedStack}
+      options={{
+        tabBarLabel: "ホーム",
+        tabBarIcon: ({color, size}) => TabBarIcon('home')
+      }}
     />
     <BottomTab.Screen
       name="SelectImage"
       component={SelectImageStack}
+      options={{
+        tabBarLabel: "新規",
+        tabBarIcon: ({color, size}) => TabBarIcon('home')
+      }}
     />
     <BottomTab.Screen
       name="MyPage"
       component={MyPageStack}
+      options={{
+        tabBarLabel: "マイページ",
+        tabBarIcon: ({color, size}) => TabBarIcon('home')
+      }}
     />
   </BottomTab.Navigator>
 )
